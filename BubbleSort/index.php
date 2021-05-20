1ef270b82c10c089e00d9201cf6c94f26d8b8a8e
TamNguyen <ngductam91@gmail.com>
2021-05-20 15:26:49 +0700

    Bubble Sort

--- BubbleSort/index.php
+++ BubbleSort/index.php
@@ -12,7 +12,7 @@ function bubbleSort($arr)
 {
     $length = count($arr);
     for ($i = 0; $i < $length; $i++) {
-        for ($j = 0; $j < $length - $i - 1; $j++) {
+        for ($j = 0; $j < $length - 1; $j++) {
             if ($arr[$j] > $arr[$j + 1]) {
                 $temp = $arr[$j + 1];
                 $arr[$j+1] = $arr[$j];
